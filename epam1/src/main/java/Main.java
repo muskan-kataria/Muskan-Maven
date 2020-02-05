79b8b1c34e0863996ef4e37d702414f0fd21d64a
muskan kataria <47684499+muskan-kataria@users.noreply.github.com>
2020-02-05 09:39:03 +0530

    Maven

--- epam1/src/main/java/Main.java
+++ epam1/src/main/java/Main.java
@@ -5,21 +5,20 @@ public class Main
 {
     public static void main(String args[])
     {
-        Sweet l1=new Ladoo("X",120,80,"Besan");
-        Sweet l2=new Ladoo("Y",110,70,"Boondi");
-        Sweet l3=new Ladoo("Z",100,50,"MotiChoor");
+        Sweet X=new Candy("X",130,80,"Gummies");
+        Sweet Y=new Candy("Y",150,70,"Cooton Candy");
+       
 
-        Sweet b1=new Barfi("A",200,50,"Badam");
-        Sweet b2=new Barfi("B",250,60,"Chocolate");
+        Sweet A=new Chocolate("A",200,50,"Wafer");
+        Sweet B=new Chocolate("B",250,60,"Nuts");
 
 
         Gift gifts=new Gift();
-        gifts.add(l1);
-        gifts.add(l2);
-        gifts.add(l3);
-
-        gifts.add(b1);
-        gifts.add(b2);
+        gifts.add(X);
+        gifts.add(Y);
+        gifts.add(A);
+        gifts.add(B);
+        
 
         System.out.println(gifts.getWeight());
         System.out.println(gifts.toString());
